ac53d9bc216ffefd9fe3d345c41069a6874ee077
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2019-06-07 14:07:52 +0100

    Up-level Conform version to 6.4.0 for release

--- Conform/My Project/AssemblyInfo.vb
+++ Conform/My Project/AssemblyInfo.vb
@@ -33,8 +33,8 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.4.63.4")>
-<Assembly: AssemblyFileVersion("6.4.63.4")>
+<Assembly: AssemblyVersion("6.4.64.0")>
+<Assembly: AssemblyFileVersion("6.4.64.0")>
 
 <Assembly: NeutralResourcesLanguageAttribute("")> 
 <Assembly: CLSCompliant(True)> 
