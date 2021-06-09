d224323ef44a2393b2871fd5911a5ad262054dac
Peter Simpson <38855929+Peter-Simpson@users.noreply.github.com>
2021-06-09 11:31:28 +0100

    Ensure that 32bit and 64bit version numbers are identical.

--- BuildConform.cmd
+++ BuildConform.cmd
@@ -6,28 +6,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 SET ConformDirectory=%cd%
 @echo Current directory: %ConformDirectory%
 
-cd %ConformDirectory%
-@echo Building Release x86 in directory %cd%
-msbuild /t:rebuild /p:configuration=Release;Platform=x86
-
-cd %ConformDirectory%
-@echo Building Release x64 in directory %cd%
-msbuild /t:rebuild /p:configuration=Release;Platform=x64
-
-cd %ConformDirectory%
-@echo Building Release AnyCPU in directory %cd%
-msbuild /t:rebuild /p:configuration=Release;Platform="Any CPU"
-
-cd %ConformDirectory%
-@echo Building Debug x86 in directory %cd%
-msbuild /t:rebuild /p:configuration=Debug;Platform=x86
-
-cd %ConformDirectory%
-@echo Building Debug x64 in directory %cd%
-msbuild /t:rebuild /p:configuration=Debug;Platform=x64
-
-cd %ConformDirectory%
-
-@echo Building Debug AnyCPU in directory %cd%
-msbuild /t:rebuild /p:configuration=Debug;Platform="Any CPU"
- 
+@echo Building Conform in directory %cd%
+msbuild Setup\buildconform.msbuild
